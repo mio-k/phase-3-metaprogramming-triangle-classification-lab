8cb4c369387d5bc4936edded22ddfb1ce96a2c71
mio kataoka <mio.kanna@gmail.com>
2022-06-28 18:56:21 -0700

    commit tree 4b59c99eed245680ff86cc6e6bec0330e18d2619

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :num1, :num2, :num3
+
+  def initialize(num1, num2, num3)
+    @num1 = num1
+    @num2 = num2
+    @num3 = num3
+  end
+
+  def kind
+    validate
+    if num1 == num2 && num2 == num3
+      :equilateral
+    elsif num1 == num2 || num2 == num3 || num3 == num1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def no_negative_numbers?
+    num1 > 0 && num2 > 0 && num3 > 0
+  end
+
+  def check_lengths?
+    num1 + num2 > num3 && num2 + num3 > num1 && num3 + num1 > num2
+  end
+
+  def validate
+    raise TriangleError unless no_negative_numbers? && check_lengths?
+  end
+
+
+  class TriangleError < StandardError
+  end
 end
